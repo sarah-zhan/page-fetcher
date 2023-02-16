d851247286d7887802ffcad9df8b62a41ef21aa1
sarah-zhan <yizh.contact@gmail.com>
2023-02-16 15:09:10 -0800

    first commit

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const fs = require('fs');
+
+const fetcher = function (source, filePath) {
+
+  request(source, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the homepage.
+    fs.writeFile(filePath, body, 'utf8', (error, content) => {
+      if (error) throw error;
+      console.log(content);
+  
+    });
+  });
+
+
+};
+  
+fetcher('http://www.example.edu', './test.txt')
+
+// fs.writeFile('./test.txt', 'testing', (err) => {
+//   if (err) throw err;
+//   console.log('It has been saved.')
+// })
+
